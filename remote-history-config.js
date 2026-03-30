afe894a4cbf22d9a6801be3870aaa0f40db97e26
Ian <you@example.com>
2026-03-30 09:51:08 -0300

    Enable the Supabase remote history config

--- remote-history-config.js
+++ remote-history-config.js
@@ -1,5 +1,5 @@
 window.DUELO_REMOTE_HISTORY_CONFIG = Object.freeze({
-  REMOTE_MATCH_HISTORY_ENABLED: false,
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  REMOTE_MATCH_HISTORY_ENABLED: true,
+  SUPABASE_URL: "https://zyeeszvncmxjyjgbdfgq.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_vMDApjM3wnRGuHLzfoZ5Gw_UXZLVYSu"
 });
